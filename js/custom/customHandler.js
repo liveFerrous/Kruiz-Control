bf589756aca94a8895bd17fa12abbb16fa36eab3
Ferrous <>
2023-03-12 14:19:11 -0600

    add images.json template

--- js/custom/customHandler.js
+++ js/custom/customHandler.js
@@ -2,29 +2,43 @@ class CustomHandler extends Handler {
     /**
      * Create a new Actions handler.
      */
-    constructor(obs, scene, images) {
+    constructor(obs, scene, imageSettings) {
       super('Custom', []);
       this.obs = obs;
 
       this.obs.on('SceneTransitionStarted', this.onSceneTransition.bind(this));
 
-      this.images = images;
-      this.rootImageFolder = images.rootImageFolder;
-      this.rootSoundFolder = images.rootSoundFolder;
+      this.images = imageSettings.images;
+      this.rootImageFolder = imageSettings.rootImageFolder;
+      this.rootSoundFolder = imageSettings.rootSoundFolder;
       this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
+      this.typeSounds = imageSettings.typeSounds;
 
       this.setCurrentScene.bind(this);
       this.setCurrentScene(scene);
     }
 
     getRandomFile(type) {
-      const typeImages = this.images[type.toLowerCase()];
+      const typeImages = this.images[type.toLowerCase()]; 
       const randomImage =  typeImages.images[Math.floor(Math.random()*typeImages.images.length)];
+      const sound = this.getSound(randomImage);
       return{
         imagePath: [this.rootImageFolder, typeImages.folder, randomImage.image].join('/'),
-        soundPath: [this.rootSoundFolder, randomImage.sound].join('/'),
-        message: randomImage.customMessage == undefined ? `This is a ${randomImage.type}` : randomImage.customMessage
+        soundPath: sound == undefined ? undefined : [this.rootSoundFolder, this.getSound(randomImage)].join('/'),
+        message: randomImage.message == undefined ? `This is a ${randomImage.type}` : randomImage.customMessage
       };
+    }
+
+    getSound(imageDef) {    
+      if (imageDef.sound != undefined) {
+       return imageDef.sound;
+      }
+      
+      if (this.typeSounds[imageDef.type] != undefined) {
+        return this.typeSounds[imageDef.type];
+      }
+
+      return undefined;
     }
 
     setCurrentScene(scene) {
@@ -73,7 +87,9 @@ class CustomHandler extends Handler {
             file: randomFile.imagePath
           })
           this.obs.setSourceVisibility(obsImageSource, true, this.currentScene);
-          this.playSound(randomFile.soundPath, duration, volume);
+          if (randomFile.soundPath != undefined) {
+            this.playSound(randomFile.soundPath, duration, volume);
+          }
           ComfyJS.Say(randomFile.message);
           await timeout(duration);
           this.obs.setSourceVisibility(obsImageSource, false, this.currentScene);
